7289104b1fb38203a2fd6b2a360d03e809f99d09
kahwata <kahwaty.anthony@gmail.com>
2020-10-24 13:23:39 -0400

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./product-list.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ProductListComponent /*also here*/],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  productList = [
+    { name: "Z900", price: 8799 },
+    { name: "shubert helmet", price: 999 },
+    { name: "sport gloves", price: 99 }
+  ];
+  cartProductList = [];
+}
