29f248315c60fb0e5b6b46410131db589abeda89
kahwata <49405110+kahwata@users.noreply.github.com>
2020-10-24 14:12:39 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./product-list.component";
 import { ShoppingCartComponent } from "./shopping-cart.component";
+import { ProductComponent } from "./product.component";
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ShoppingCartComponent, ProductListComponent /*also here*/],
+  declarations: [AppComponent,ProductComponent, ShoppingCartComponent, ProductListComponent /*also here*/],
   bootstrap: [AppComponent]
 })
 export class AppModule {
