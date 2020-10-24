fbe612ba7b192cff7a151b5274254656c9a6d7bc
kahwata <kahwaty.anthony@gmail.com>
2020-10-24 13:30:31 -0400

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  productList = [
+    { name: "Z900", price: 8799 },
+    { name: "shubert helmet", price: 999 },
+    { name: "sport gloves", price: 99 }
+  ];
+  cartProductList = [];
 }
